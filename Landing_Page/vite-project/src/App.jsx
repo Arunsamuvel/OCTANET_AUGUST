76659e86b279b3420f194be15c6a9ef9e5152947
Arunsamuvel <arun15ee03@gmail.com>
2024-08-01 19:35:45 +0530

    App.jsx variables declared

--- Landing_Page/vite-project/src/App.jsx
+++ Landing_Page/vite-project/src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import Background from "./component/Background/Background";
+
 const App = () => {
-  return <div>
-    
-  </div>;
+  // created the variable for let hero data with 3 objects for we have display for the value
+  let heroData = [
+    {
+      text1: "Dive Into",
+      text2: "What You Love",
+    },
+    {
+      text1: "Indulge",
+      text2: "your passion",
+    },
+    {
+      text1: "Give into",
+      text2: "passion",
+    },
+  ];
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = false;
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+    </div>
+  );
 };
 
 export default App;
